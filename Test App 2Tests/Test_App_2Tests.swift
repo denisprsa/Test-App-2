18c923204fc8b78677d41256805d9e7221b50d98
Denis Prsa <denisprrs@gmail.com>
2016-02-16 17:01:06 +0100

    Initial Commit

---
+++ Test App 2Tests/Test_App_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Test_App_2Tests.swift
+//  Test App 2Tests
+//
+//  Created by Denis Prša on 16. 02. 16.
+//  Copyright © 2016 Denis Prša. All rights reserved.
+//
+
+import XCTest
+@testable import Test_App_2
+
+class Test_App_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
