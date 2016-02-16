132a3ce7121d0511374f4edee3bb6fc859591044
Denis Prša <denisprrs@gmail.com>
2016-02-16 17:19:59 +0100

    blabla

--- Test App 2/ViewController.swift
+++ Test App 2/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    func id(id: Double)->Double{
+        
+        return 0.0;
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
